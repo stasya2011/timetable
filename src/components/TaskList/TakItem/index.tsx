caff9b0cf2c366f6923286c543886c5d1b10c0fd
Stasya Geleisha <s.geleisha@gmail.com>
2024-01-11 12:54:53 +0200

    add style for selectors

--- src/components/TaskList/TakItem/index.tsx
+++ src/components/TaskList/TakItem/index.tsx
@@ -1,4 +1,4 @@
-import { Badge, BadgeProps, Button } from "antd";
+import { Badge, BadgeProps, Button, Select, Space } from "antd";
 import { IListData, IState } from "../../../redux/types";
 import { useAppDispatch } from "../../../redux/store";
 import { deleteEvent, deleteItem } from "../../../redux/slice/eventsSlice";
@@ -15,18 +15,36 @@ const TaskItem = ({ item, event }: { item: IListData; event: IState }) => {
     dispatch(deleteEvent({ eventId: event.eventId }));
   };
 
+  const handleChange = (value: string) => {
+    console.log(`+++ selected ${value}`);
+  };
+
   return (
     <li key={item.id}>
       <Badge status={item.type as BadgeProps["status"]} text={item.content} />
-      <Button
-        onClick={() => {
-          deleteItemFromList(item);
-        }}
-        shape="circle"
-        className={styles.deleteBtn}
-      >
-        &#10007;
-      </Button>
+      <div className={styles.wrapper}>
+        <Button
+          onClick={() => {
+            deleteItemFromList(item);
+          }}
+          shape="circle"
+          className={styles.deleteBtn}
+        >
+          &#10007;
+        </Button>
+
+        <Select
+          className={styles.selectors}
+          defaultValue="success"
+          style={{ width: 80 }}
+          onChange={handleChange}
+          options={[
+            { value: "success", label: "success" },
+            { value: "processing", label: "processing" },
+            { value: "default", label: "default" },
+          ]}
+        />
+      </div>
     </li>
   );
 };
