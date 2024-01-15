575f91e63d0aae5555c5ec438c077096c8881ab7
Stasya Geleisha <s.geleisha@gmail.com>
2024-01-15 16:16:42 +0200

    Add a temporary variant of rendering data based on events.

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,8 +1,101 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
-const ChartComponent = ({ data }: { data: any }) => {
+export enum STATUS {
+  TODO = "Todo",
+  PROCESSING = "Processing",
+  DONE = "Done",
+}
+
+interface IChartElement {
+  type: string;
+  content: string;
+  id: string;
+}
+
+interface IDataset {
+  todo: IChartElement[];
+  done: IChartElement[];
+  proccess: IChartElement[];
+}
+
+interface IChart {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string[];
+  }[];
+}
+
+const init_data: IChart = {
+  labels: [STATUS.TODO, STATUS.PROCESSING, STATUS.DONE],
+  datasets: [
+    {
+      label: "Month",
+      data: [],
+      backgroundColor: ["green", "red", "grey"],
+    },
+  ],
+};
+
+const ChartComponent = () => {
   const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const { events } = useSelector((state: RootState) => state);
+  const [data, setData] = useState<IChart>(init_data);
+  const [dataset, setDataset] = useState<IDataset>({
+    todo: [],
+    done: [],
+    proccess: [],
+  });
+
+  useEffect(() => {
+    events.forEach((event) => {
+      event.listData.forEach((element) => {
+        switch (element.type) {
+          case "success":
+            setDataset((preState) => {
+              const copyTodoEl = JSON.parse(JSON.stringify(preState));
+              copyTodoEl.todo.push(element);
+              return copyTodoEl;
+            });
+            break;
+          case "processing":
+            setDataset((preState) => {
+              const copyTodoEl = JSON.parse(JSON.stringify(preState));
+              copyTodoEl.proccess.push(element);
+              return copyTodoEl;
+            });
+            break;
+          case "default":
+            setDataset((preState) => {
+              const copyTodoEl = JSON.parse(JSON.stringify(preState));
+              copyTodoEl.done.push(element);
+              return copyTodoEl;
+            });
+            break;
+          default:
+            return;
+        }
+      });
+    });
+
+    setData((prevState) => {
+      const updatedState = JSON.parse(JSON.stringify(prevState));
+      const updatedData = [
+        dataset.todo.length,
+        dataset.proccess.length,
+        dataset.done.length,
+      ];
+      updatedState.datasets[0].data = updatedData;
+
+      return updatedState;
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     const ctx = chartRef.current ? chartRef.current.getContext("2d") : null;
@@ -17,7 +110,8 @@ const ChartComponent = ({ data }: { data: any }) => {
         myChart.destroy();
       };
     }
-  }, [data]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataset]);
 
   return (
     <div style={{ width: 500 }}>
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -4,21 +4,11 @@ import CalendarPage from "../CalendarPage";
 import ErrorPage from "../ErrorPage";
 import styles from "./mainPage.module.scss";
 
-const chartData = {
-  labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [10, 20, 30, 25],
-      backgroundColor: ["red", "green", "blue", "pink"],
-    },
-  ],
-};
 const MainPage = () => {
   return (
     <main className={styles.main}>
       <Routes>
-        <Route element={<ChartComponent data={chartData} />} path="/diagram" />
+        <Route element={<ChartComponent />} path="/diagram" />
         <Route element={<CalendarPage />} path="/" />
         <Route element={<ErrorPage />} path="*" />
       </Routes>
